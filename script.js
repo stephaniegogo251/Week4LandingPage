54d896a0def7858c4ca5953787ea0bf6af088e56
Stephanie Sunil <137881129+stephaniegogo251@users.noreply.github.com>
2024-07-30 10:54:29 +0400

    front end week  4 task

---
+++ script.js
@@ -0,0 +1,5 @@
+const timings = document.getElementById("change");
+const timer = document.getElementById("timer");
+timer.addEventListener("click", function() {
+    timings.innerText = "Timing: 8:30am - 12:00am (❁´◡`❁)";
+});
